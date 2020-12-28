54a8f3ff3f4e668729e592f3cd95c2524965ebfc
sethuramv <sethuram.v@appoets.com>
2020-12-28 19:02:44 +0530

    Updated Code

--- Project/VIPER/View/Controllers/StoryBoards/ViewControllers/Profile/EditProfileTableViewController.swift
+++ Project/VIPER/View/Controllers/StoryBoards/ViewControllers/Profile/EditProfileTableViewController.swift
@@ -77,7 +77,9 @@ class EditProfileTableViewController: UITableViewController {
             if let fileURL = self.videoURL {
                 if let videoData = NSData(contentsOf: fileURL as URL) {
                        print(videoData.length)
-                    self.videoData = videoData
+//                    self.videoData = videoData
+                  self.encodeVideo(videoURL: self.videoURL! as URL)
+                   
                    }
                }
             do {
@@ -387,4 +389,65 @@ extension EditProfileTableViewController  {
         }
         self.dismiss(animated: true, completion: nil)
     }
+    
+    func encodeVideo(videoURL: URL){
+        let avAsset = AVURLAsset(url: videoURL)
+        let startDate = Date()
+        let exportSession = AVAssetExportSession(asset: avAsset, presetName: AVAssetExportPresetPassthrough)
+
+        let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let myDocPath = NSURL(fileURLWithPath: docDir).appendingPathComponent("temp.mp4")?.absoluteString
+
+        let docDir2 = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as NSURL
+
+        let filePath = docDir2.appendingPathComponent("rendered-Video.mp4")
+        deleteFile(filePath!)
+
+        if FileManager.default.fileExists(atPath: myDocPath!){
+            do{
+                try FileManager.default.removeItem(atPath: myDocPath!)
+            }catch let error{
+                print(error)
+            }
+        }
+
+        exportSession?.outputURL = filePath
+        exportSession?.outputFileType = AVFileType.mp4
+        exportSession?.shouldOptimizeForNetworkUse = true
+
+        let start = CMTimeMakeWithSeconds(0.0, preferredTimescale: 0)
+        let range = CMTimeRange(start: start, duration: avAsset.duration)
+        exportSession?.timeRange = range
+
+        exportSession!.exportAsynchronously{() -> Void in
+            switch exportSession!.status{
+            case .failed:
+                print("\(exportSession!.error!)")
+            case .cancelled:
+                print("Export cancelled")
+            case .completed:
+                let endDate = Date()
+                let time = endDate.timeIntervalSince(startDate)
+                print(time)
+                print("Successful")
+                print(exportSession?.outputURL ?? "")
+               
+            default:
+                break
+            }
+        }
+//        return ""
+    }
+
+    func deleteFile(_ filePath:URL) {
+        guard FileManager.default.fileExists(atPath: filePath.path) else{
+            return
+        }
+        do {
+            try FileManager.default.removeItem(atPath: filePath.path)
+        }catch{
+            fatalError("Unable to delete file: \(error) : \(#function).")
+        }
+    }
+
 }
